0b82f45b84be098f694edfe7a1c6556bf0d206ee
mosup16 <mosup16@gmail.com>
2021-04-25 16:27:08 +0200

    adding a filter that checks if the input is greater than 20 before subtracting a ten from it

--- src/main/java/functionalprogramming/practice/Session2.java
+++ src/main/java/functionalprogramming/practice/Session2.java
@@ -7,12 +7,20 @@ public class Session2 {
         // functional implementation
         System.out.println("#Functional");
         var data = List.of(1, 3, 6, 7, 14, 9);
-        data.stream().map(i -> i+1).map(i -> i * i).map(i -> i - 10).forEach(System.out::println);
+        data.stream().map(i -> i+1).map(i -> i * i).filter(i -> i > 20).map(i -> i - 10).forEach(System.out::println);
 
         // imperative
         System.out.println("#Imperative");
+        // not really a clean code
         for (int x : data){
-            System.out.println(subtractTen(square(addOne(x))));
+            printAndSubtractTenIfGreaterThan(20,square(addOne(x)));
+        }
+    }
+
+
+    private static void printAndSubtractTenIfGreaterThan(int compareTo, int num) {
+        if (num > compareTo){
+            System.out.println(subtractTen(num));
         }
     }
 
