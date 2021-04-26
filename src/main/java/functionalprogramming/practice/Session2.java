9df16ee9dfec56a7c93fea7cb51e89e7b26c1a07
mosup16 <mosup16@gmail.com>
2021-04-26 21:00:43 +0200

    printing just the largest two results of the previous result set

--- src/main/java/functionalprogramming/practice/Session2.java
+++ src/main/java/functionalprogramming/practice/Session2.java
@@ -1,38 +1,63 @@
 package functionalprogramming.practice;
 
-import java.util.List;
+import java.util.*;
+import java.util.function.Function;
 
 public class Session2 {
+
+
+    private static final Comparator<Integer> reverseSortStrategy = (i, j) -> -1 * Integer.compare(i, j);
+
     public static void main(String[] args) {
-        // functional implementation
-        System.out.println("#Functional");
         var data = List.of(1, 3, 6, 7, 14, 9);
-        data.stream().map(i -> i+1).map(i -> i * i).filter(i -> i > 20).map(i -> i - 10).forEach(System.out::println);
+        System.out.println("#Functional");
+        executeFunctionalExample(data, 20, 2);
 
-        // imperative
         System.out.println("#Imperative");
-        // not really a clean code
-        for (int x : data){
-            printAndSubtractTenIfGreaterThan(20,square(addOne(x)));
+        executeImperativeExample(data, 20, 2);
+    }
+
+    private static void executeFunctionalExample(List<Integer> data, int maxAllowed, int limit) {
+        data.stream().map(i -> i + 1).map(i -> i * i)
+                .filter(i -> i > maxAllowed).sorted(reverseSortStrategy).limit(limit)
+                .map(i -> i - 10).forEach(System.out::println);
+    }
+
+
+    private static void executeImperativeExample(List<Integer> data, int maxAllowed, int limit) {
+
+        PriorityQueue<Integer> squaredNums = new PriorityQueue<>(reverseSortStrategy);
+        for (int x : data) {
+            x = square(addOne(x));
+            if (x > maxAllowed) {
+                squaredNums.add(subtractTen(x));
+            }
+        }
+        for (int greatestNum : getGreatestNums(squaredNums, limit)) {
+            System.out.println(greatestNum);
         }
     }
 
 
-    private static void printAndSubtractTenIfGreaterThan(int compareTo, int num) {
-        if (num > compareTo){
-            System.out.println(subtractTen(num));
+    private static LinkedList<Integer> getGreatestNums(PriorityQueue<Integer> queue, int limit) {
+        int i = 0;
+        var list = new LinkedList<Integer>();
+        while (!queue.isEmpty() && i < limit) {
+            list.add(queue.poll());
+            i++;
         }
+        return list;
     }
 
-    private static int addOne(int x){
+    private static int addOne(int x) {
         return x + 1;
     }
 
-    private static int square(int x){
+    private static int square(int x) {
         return x * x;
     }
 
-    private static int subtractTen(int x){
+    private static int subtractTen(int x) {
         return x - 10;
     }
 }
